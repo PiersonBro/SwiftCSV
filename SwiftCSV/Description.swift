cc008d40a6f1d349b4000861f1ff76f6fed09a32
Ezekiel Pierson <ezekiel.m.pierson@gmail.com>
2019-10-06 01:36:21 -0700

    Finish merge

--- SwiftCSV/Description.swift
+++ SwiftCSV/Description.swift
@@ -12,11 +12,7 @@ extension CSV: CustomStringConvertible {
     public var description: String {
         let head = header.joined(separator: ",") + "\n"
         
-<<<<<<< HEAD
-        let cont = rows.map { row in
-=======
         let cont = namedRows.map { row in
->>>>>>> 22dc4dd1272e990da64ea87a8bc84bb606eb177e
             header.map { row[$0]! }.joined(separator: ",")
         }.joined(separator: "\n")
         return head + cont
